9c05775f53d2d81caa989fcaafddf6aecab48196
atharvmohanjadhav <atharvjadhav2910@gmail.com>
2025-06-06 21:39:55 +0530

    updated

---
+++ main.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from src.train_schedule import TrainSchedule
+from src.live_train_status import LiveTrainStatus
+
+class IRCTC:
+    def __init__(self):
+        user_ip = input("""
+        1. Enter 1 to check live location of train.
+        2. Enter 2 to check PNR.
+        3. Enter 3 to check train status.         
+            """)
+        
+        if user_ip == "1":
+            LiveTrainStatus().get_live_status()
+        elif user_ip == "2":
+            pass
+        else:
+            TrainSchedule().train_schedule_info()
+            
+
+    
+obj = IRCTC()
---
+++ src/live_train_status.py
@@ -0,0 +1,20 @@
+import requests 
+import os
+from dotenv import load_dotenv
+
+class LiveTrainStatus:
+    def __init__(self) -> None:
+        pass
+
+    def get_live_status(self):
+        train_no = input("Enter train number:")
+        date = input("Enter date (yyyymmdd):")
+        self.get_info(train_number=train_no,date=date)
+
+    def get_info(self,train_number,date):
+        API_KEY = os.getenv("API_KEY")
+        info = requests.get(f"http://indianrailapi.com/api/v2/livetrainstatus/apikey/{API_KEY}/trainnumber/{train_number}/date/{date}/")
+
+        info = info.json()
+        print(info['Message'])
+    
