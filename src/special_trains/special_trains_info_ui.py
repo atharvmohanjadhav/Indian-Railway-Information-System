5197b545dbfb7a79a1eafbb3f0fc88869752a30a
atharvmohanjadhav <atharvjadhav2910@gmail.com>
2025-06-12 17:46:34 +0530

    updated

---
+++ src/special_trains/special_trains_info_ui.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+from utils.custom_exception import IrisException
+import sys
+from src.special_trains.special_trains_info import SpeacialTrains
+
+class SpeacialTrainsUI:
+
+    def __init__(self):
+        try:
+            info = SpeacialTrains().special_trains_info()
+            if info and isinstance(info, (list, dict)) and len(info) > 0:
+                st.dataframe(info, use_container_width=True)
+            else:
+                st.json({
+                    "ResponseCode": "202",
+                    "Message": "Server busy. Try again after 5 Min."
+                })
+
+        except IrisException as e:
+            raise (e,sys)
+        
