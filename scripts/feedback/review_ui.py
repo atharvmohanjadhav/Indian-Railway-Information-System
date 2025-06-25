17bf1f3e7ef97df581624727217d4989e7a46c34
atharvmohanjadhav <atharvjadhav2910@gmail.com>
2025-06-25 23:20:55 +0530

    updated

--- scripts/feedback/review_ui.py
+++ scripts/feedback/review_ui.py
@@ -2,6 +2,7 @@ from scripts.feedback.review import get_feedback
 import streamlit as st
 
 class Feedback:
+    
     def __init__(self):
         text = st.sidebar.text_area("Give your feedback")
         if st.sidebar.button("Feedback"):
