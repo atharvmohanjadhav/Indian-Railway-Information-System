53f7dfaf015b9ef49f1208c3fd3b1890492fbbe2
atharvmohanjadhav <atharvjadhav2910@gmail.com>
2025-07-02 18:58:20 +0530

    update rail news

--- scripts/news/rail_news.py
+++ scripts/news/rail_news.py
@@ -1,22 +1,27 @@
 import requests
 from dotenv import load_dotenv
 import os
+from utils.custom_exception import IrisException
+import sys
 
 load_dotenv()
 
 def fetch_railway_news():
-    api_key = os.getenv("NEWS_API_KEY")
-    url = (
-        f"https://newsapi.org/v2/everything?"
-        f"q=Indian%20Railways&"
-        f"sortBy=publishedAt&"
-        f"language=en&"
-        f"apiKey={api_key}"
-    )
+    try:
+        api_key = os.getenv("NEWS_API_KEY")
+        url = (
+            f"https://newsapi.org/v2/everything?"
+            f"q=Indian%20Railways&"
+            f"sortBy=publishedAt&"
+            f"language=en&"
+            f"apiKey={api_key}"
+        )
 
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        return data["articles"][:10] 
-    else:
-        return []
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data["articles"][:10] 
+        else:
+            return []
+    except Exception as e:
+        raise IrisException(e,sys)
--- scripts/news/rail_news_ui.py
+++ scripts/news/rail_news_ui.py
@@ -1,22 +1,26 @@
 import streamlit as st
 from scripts.news.rail_news import fetch_railway_news
+from utils.custom_exception import IrisException
+import sys
 
 class RunNewsChain:
     def __init__(self) -> None:
-        st.subheader("📰 Latest Indian Railway News")
+        try:
+            st.subheader("📰 Latest Indian Railway News")
 
-        news_list = fetch_railway_news()
+            news_list = fetch_railway_news()
 
-        if news_list:
-            for article in news_list:
-                if article.get('urlToImage'):
-                    st.image(article['urlToImage'],width=300)
-                
-                st.markdown(f"### [{article['title']}]({article['url']})")
-                st.write(f"**Source:** {article['source']['name']}  \n**Published:** {article['publishedAt'][:10]}")
-                st.write(article['description'] or "No description available.")
-                st.write("---")
-        else:
-            st.warning("Sorry, couldn't fetch the latest news at the moment. Please try again later!")
+            if news_list:
+                for article in news_list:
+                    if article.get('urlToImage'):
+                        st.image(article['urlToImage'],width=300)
+                    
+                    st.markdown(f"### [{article['title']}]({article['url']})")
+                    st.write(f"**Source:** {article['source']['name']}  \n**Published:** {article['publishedAt'][:10]}")
+                    st.write(article['description'] or "No description available.")
+                    st.write("---")
+            else:
+                st.warning("Sorry, couldn't fetch the latest news at the moment. Please try again later!")
 
-
+        except Exception as e:
+            raise IrisException(e,sys)
